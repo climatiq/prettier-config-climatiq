6c4ce6e185ed7bbe5a56d1526a753208c44116de
Joe Pantazidis <joe@climatiq.io>
2026-04-28 09:55:45 +0200

    feat: extend config with de facto additional rules concerning tab width = 4 spaces

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+/**
+ * @type {import('prettier').Config}
+ */
 module.exports = {
   "semi": true,
   "trailingComma": "all",
   "singleQuote": true,
-  "printWidth": 100
+  "printWidth": 100,
+  "endOfLine": "lf",
+  "tabWidth": 4,
+  "useTabs": false,
 }
