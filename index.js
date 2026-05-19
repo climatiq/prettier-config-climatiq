efbb0a477a350a31d7e41b219f696438e030307c
Lorenzo Romero <lorenzo@climatiq.io>
2026-05-19 13:59:16 +0200

    Added pre-commit hook with husky and CI check for formatting

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @type {import('prettier').Config}
  */
 module.exports = {
-  "semi": true,
-  "trailingComma": "all",
-  "singleQuote": true,
-  "printWidth": 100,
-  "endOfLine": "lf",
-  "tabWidth": 4,
-  "useTabs": false,
-}
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 100,
+  endOfLine: "lf",
+  tabWidth: 4,
+  useTabs: false,
+};
